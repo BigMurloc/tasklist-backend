b7c380787003b78457b3e6681e9630ee4b333082
BigMurloc <s21159@pjwstk.edu.pl>
2021-02-25 15:15:50 +0100

    feat: data binding using model mapper

--- src/main/java/pl/tasklist/tasklistbackend/controller/UserController.java
+++ src/main/java/pl/tasklist/tasklistbackend/controller/UserController.java
@@ -1,10 +1,12 @@
 package pl.tasklist.tasklistbackend.controller;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import pl.tasklist.tasklistbackend.dto.UserDTO;
 import pl.tasklist.tasklistbackend.entity.User;
 import pl.tasklist.tasklistbackend.exception.UserAlreadyExistsException;
 import pl.tasklist.tasklistbackend.service.UserService;
@@ -14,15 +16,22 @@ public class UserController {
 
 
     private final UserService userService;
+    private final ModelMapper modelMapper;
 
-    public UserController(UserService userService) {
+    public UserController(UserService userService, ModelMapper modelMapper) {
         this.userService = userService;
+        this.modelMapper = modelMapper;
     }
 
     @PostMapping("/user/register")
-    public ResponseEntity<?> save(@RequestBody User user) throws UserAlreadyExistsException {
-        userService.save(user);
+    public ResponseEntity<?> save(@RequestBody UserDTO userDTO) throws UserAlreadyExistsException {
+        userService.save(convertToEntity(userDTO));
         return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+    private User convertToEntity(UserDTO userDTO){
+        User user = modelMapper.map(userDTO, User.class);
+        return user;
     }
 
 
