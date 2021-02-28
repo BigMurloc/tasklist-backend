60075702b0d7db8dabbf71428743a7cfacfeec32
BigMurloc <s21159@pjwstk.edu.pl>
2021-02-28 11:28:58 +0100

    feat: login controller logic

--- src/main/java/pl/tasklist/tasklistbackend/controller/LoginController.java
+++ src/main/java/pl/tasklist/tasklistbackend/controller/LoginController.java
@@ -1,11 +1,27 @@
 package pl.tasklist.tasklistbackend.controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import pl.tasklist.tasklistbackend.dto.UserLoginDTO;
+import pl.tasklist.tasklistbackend.exception.UnauthorizedException;
+import pl.tasklist.tasklistbackend.service.AuthenticationService;
 
 @RestController
 public class LoginController {
 
 
+    private final AuthenticationService authenticationService;
+
+    public LoginController(AuthenticationService authenticationService) {
+        this.authenticationService = authenticationService;
+    }
+
+    @PostMapping("/login")
+    public void login(@RequestBody UserLoginDTO userLoginDTO) throws UnauthorizedException {
+        if(!authenticationService.login(userLoginDTO))
+            throw new UnauthorizedException();
+    }
 
 
 }
--- src/main/java/pl/tasklist/tasklistbackend/service/AuthenticationService.java
+++ src/main/java/pl/tasklist/tasklistbackend/service/AuthenticationService.java
@@ -1,10 +1,19 @@
 package pl.tasklist.tasklistbackend.service;
 
 import org.springframework.stereotype.Service;
+import pl.tasklist.tasklistbackend.dto.UserLoginDTO;
+import pl.tasklist.tasklistbackend.repository.UserRepository;
 
 @Service
 public class AuthenticationService {
 
+    private final UserRepository userRepository;
 
+    public AuthenticationService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 
+    public boolean login(UserLoginDTO userLoginDTO) {
+        return false;
+    }
 }
