d9039faedd96f47b9a87c7c1985ca3adbe27d262
BigMurloc <s21159@pjwstk.edu.pl>
2021-02-28 11:49:25 +0100

    feat: UserService logic

--- src/main/java/pl/tasklist/tasklistbackend/service/UserService.java
+++ src/main/java/pl/tasklist/tasklistbackend/service/UserService.java
@@ -1,10 +1,15 @@
 package pl.tasklist.tasklistbackend.service;
 
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
-    public boolean matches(String rawPassword, String hashedPassword) {
-        return false;
+
+    private final PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+
+    public boolean matches(String rawPassword, String encodedPassword) {
+        return passwordEncoder.matches(rawPassword, encodedPassword);
     }
 }
