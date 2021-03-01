c7734ae45356989009ed8cf7da0ce8dbbd84ec25
BigMurloc <s21159@pjwstk.edu.pl>
2021-03-01 14:39:37 +0100

    feat(task): TaskService logic

--- src/main/java/pl/tasklist/tasklistbackend/service/TaskService.java
+++ src/main/java/pl/tasklist/tasklistbackend/service/TaskService.java
@@ -1,9 +1,11 @@
 package pl.tasklist.tasklistbackend.service;
 
 import org.modelmapper.ModelMapper;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import pl.tasklist.tasklistbackend.dto.TaskDTO;
 import pl.tasklist.tasklistbackend.entity.Task;
+import pl.tasklist.tasklistbackend.entity.User;
 import pl.tasklist.tasklistbackend.repository.TaskRepository;
 
 import java.util.List;
@@ -26,6 +28,7 @@ public class TaskService {
 
     public void update(Long id, TaskDTO taskDTO) {
         Task task = taskRepository.findById(id);
+        modelMapper.map(taskDTO, task);
         taskRepository.update(task);
     }
 
@@ -34,7 +37,8 @@ public class TaskService {
     }
 
     public List<Task> getAll() {
-        return taskRepository.getAll();
+        User currentUser = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        return taskRepository.getAll(currentUser.getId());
     }
 
     private Task convertToEntity(TaskDTO taskDTO){
