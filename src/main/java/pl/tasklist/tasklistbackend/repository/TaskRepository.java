6357ffef7df451212fb8a40adba83b4f32943f20
BigMurloc <s21159@pjwstk.edu.pl>
2021-03-01 14:39:21 +0100

    feat(task): TaskRepository logic

--- src/main/java/pl/tasklist/tasklistbackend/repository/TaskRepository.java
+++ src/main/java/pl/tasklist/tasklistbackend/repository/TaskRepository.java
@@ -3,24 +3,38 @@ package pl.tasklist.tasklistbackend.repository;
 import org.springframework.stereotype.Repository;
 import pl.tasklist.tasklistbackend.entity.Task;
 
+import javax.persistence.EntityManager;
 import java.util.List;
 
 @Repository
 public class TaskRepository {
+    private final EntityManager entityManager;
+
+    public TaskRepository(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
     public void save(Task task) {
+        entityManager.persist(task);
     }
 
     public void update(Task task) {
+        entityManager.merge(task);
     }
 
-    public Task findById(Long id) {
-        return null;
+    public Task findById(Long primaryKey) {
+        return entityManager.find(Task.class, primaryKey);
     }
 
-    public void delete(Long id) {
+    public void delete(Long primaryKey) {
+        entityManager.remove(findById(primaryKey));
     }
 
-    public List<Task> getAll() {
-        return null;
+    public List<Task> getAll(Long creator) {
+        String query = "SELECT task FROM Task task WHERE task.user.id =: creator";
+        return entityManager
+                .createQuery(query)
+                .setParameter("creator", creator)
+                .getResultList();
     }
 }
